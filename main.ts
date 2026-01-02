c0b9f7254a9da6ebac9533793aa687a3cd1a6b13
tanishktiwari1 <tanishk@ilizien.com>
2026-01-02 10:57:31 +0530

    Initial commit

---
+++ main.ts
@@ -0,0 +1,14 @@
+import '@angular/compiler';
+import { provideHttpClient } from '@angular/common/http';
+import { enableProdMode } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+if ((window as any).ENABLE_PROD_MODE) {
+    enableProdMode();
+}
+
+const app = bootstrapApplication(AppComponent, {
+    providers: [provideHttpClient()],
+});
